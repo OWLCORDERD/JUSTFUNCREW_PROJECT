631776df72cce67f7b4fdd555ab65eeeecdcc03a
MIYEOK <lim5570@naver.com>
2022-03-23 12:00:46 +0900

    3/23 commit

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,6 +5,8 @@ import member1 from '../../assets/이준명 프로필.jpg'
 import member2 from '../../assets/정우석 프로필.jpg'
 import member3 from '../../assets/송우석 프로필.jpg'
 import member4 from '../../assets/서정욱 프로필.JPG'
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiOutlineYoutube} from 'react-icons/ai'
 
 const Profile = () => {
   return (
@@ -13,6 +15,14 @@ const Profile = () => {
     <div className = "Profile_container">
       <div className = "Profile_Director">
         <img src = {Director} alt = "director"></img>
+        <div className= "Director_content">
+        <h1>대표</h1>
+        <h1>정한별</h1>
+        <div className= "Director_social">
+        <a href = "#"><AiOutlineInstagram /></a>
+        <a href = "#"><AiOutlineYoutube/></a>
+        </div>
+        </div>
       </div>
 
       <div className= "Profile_member">
